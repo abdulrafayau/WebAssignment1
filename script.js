1600857c3b052012e792caeba038ab7d8a448766
abdulrafayau <abdulrafaydevau@gmail.com>
2026-02-28 11:27:58 +0500

    styling and js

---
+++ script.js
@@ -0,0 +1,29 @@
+// Dark Mode Toggle
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+    localStorage.setItem("theme",
+        document.body.classList.contains("dark-mode") ? "dark" : "light"
+    );
+}
+
+// Load saved theme
+window.onload = function () {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+};
+
+// Scroll To Top
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
